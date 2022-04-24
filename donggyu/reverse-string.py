05593dd585b9cc3bbf543e797b2ceee21f9d981d
donggyu <dongury1114@gmail.com>
2022-04-24 14:26:25 +0900

    2022.04.24

--- donggyu/reverse-string.py
+++ donggyu/reverse-string.py
@@ -1,16 +1,18 @@
 #sol.01
 class Solution:
     def reverseString(self, s: List[str]) -> None:
+        s.reverse() # s[:] = s[::-1]
+
+
+#sol.02
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
         start, end = 0, len(s)-1
         while start < end:
             s[start], s[end] = s[end], s[start]
             start += 1
             end -= 1
 
-#sol.02
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        s.reverse() # s[:] = s[::-1]
 
 #sol.03
 class Solution:
