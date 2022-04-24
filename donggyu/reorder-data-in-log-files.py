213834e9ca6d066d5ae2eb4d4bcf09d751d5cad3
donggyu <dongury1114@gmail.com>
2022-04-24 14:49:01 +0900

    2022.04.24

--- donggyu/reorder-data-in-log-files.py
+++ donggyu/reorder-data-in-log-files.py
@@ -1,3 +1,4 @@
+#sol.1 내 풀이(lambda 구현은 참조)
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letters, digits = [], []
@@ -12,3 +13,18 @@ class Solution:
 
 #문자로 구성된 로그가 우선
     #같은 로그이면 식별자 우선순위 [lamda식 사용하자]
+
+#sol.2
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters=[]
+        digits=[]
+        for log in logs:
+            if log[-1].isdigit(): #숫자가 포함된 로그는 logs[-1]도 숫자기 때문에 굳이 복잡하게 split을 하지 않아도 됐음
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters = sorted(letters, key=lambda letter: (letter.split()[1:],letter.split()[0])) #1 다중키 개념 1)알파벳순, 2) 식별자순
+        return letters+digits
+
+
+#https://ubermensch-with.tistory.com/644
