7078e749b0e46786f289d19e6e4199cb1f28268a
donggyu <dongury1114@gmail.com>
2022-04-26 14:16:42 +0900

    2022.04.26

--- donggyu/2022.04.24 #1/#1.test.py
+++ donggyu/2022.04.24 #1/#1.test.py
@@ -1,27 +1,23 @@
 import re
 from collections import defaultdict
-from unittest import result
 
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = ["hit"]
 
 arr = re.sub('[^0-9a-zA-Z]', ' ',paragraph)
-arr = list(arr.split())
+arr = list(arr.lower().split())
 graph = defaultdict(int)
 banned = list(map(lambda x: x.lower(), banned))
 
 for i in arr:
-    i = i.lower()
     if not i in banned:
         graph[i] += 1
-
 
 max = 0
 result = ''
 
-for i, v in graph.items():
+for k, v in graph.items():
     if v > max:
         max = v
-        result = i
-
+        result = k
 print(result)
